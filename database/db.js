3acf02d52ab7a7ebc4ea34755f0dae78753dd35a
jose esteves <josefelixestevenpacheco@gmail.com>
2022-10-20 03:25:38 -0400

    db

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const clientDB =  mongoose.connect(process.env.URI).then((m)=>{
-  // console.log('db connectado 💻')
+  console.log('db connectado 💻')
   return m.connection.getClient()
 }).catch((e)=>console.log('error en la connecion 🤦‍♀️', e))
 module.exports = clientDB  
